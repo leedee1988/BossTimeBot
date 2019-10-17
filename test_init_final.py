9c81d92702cfc6d6a779c95b8940b5f015a8af95
leedee1988 <51352979+leedee1988@users.noreply.github.com>
2019-10-17 16:51:33 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -746,11 +746,12 @@ while True:
 				repo.update_file(contents.path, "test_setting", result_textCH, contents.sha)
 
 			await client.get_channel(channel).send('< 텍스트채널 [' + client.get_channel(channel).name + '] 접속완료>', tts=False)
-            chflg = 1
 				
 			if basicSetting[6] != "":
 				#print ('join channel')
+                await client.get_channel(channel).send('1', tts=False)
 				await JointheVC(client.get_channel(basicSetting[6]), channel)
+                await client.get_channel(channel).send('2', tts=False)
 				await client.get_channel(channel).send('< 음성채널 [' + client.get_channel(basicSetting[6]).name + '] 접속완료>', tts=False)
 
 			await client.get_channel(channel).send('< 보탐봇 재시작 설정시간 ' + basicSetting[4] + '시 ' + basicSetting[5] + '분입니다. >', tts=False)
