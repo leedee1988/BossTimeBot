cf386828df7e56165320d261ff34df3d6a1bcf0a
leedee1988 <51352979+leedee1988@users.noreply.github.com>
2020-07-30 17:40:28 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1036,6 +1036,67 @@ def handle_exit():
 		except asyncio.CancelledError:
 			pass
 
+def is_manager():
+	async def pred(ctx : commands.Context) -> bool:
+		user_info : dict = ctx.bot.db.jungsan.member.find_one({"_id":ctx.author.id})
+		if not user_info:
+			return False
+		if "manager" in user_info["permissions"]:
+			return True
+		return False
+	return commands.check(pred)
+
+#서버(길드) 정보 
+def get_guild_channel_info_sungsan(bot):
+	text_channel_name : list = []
+	text_channel_id : list = []
+	
+	for guild in bot.guilds:
+		for text_channel in guild.text_channels:
+			text_channel_name.append(text_channel.name)
+			text_channel_id.append(str(text_channel.id))
+
+	return text_channel_name, text_channel_id
+
+#detail embed
+def get_detail_embed(info : dict = {}):
+	# "_id" : int = 순번
+	# "regist_ID" : str = 등록자ID
+	# "regist" : str = 등록자 겜 ID
+	# "getdate" : datetime = 등록날짜
+	# "boss" : str = 보스명
+	# "item" : str = 아이템명
+	# "toggle" : str = 루팅자 게임 ID
+	# "toggle_ID" : str = 루팅자ID
+	# "itemstatus" : str = 아이템상태(미판매, 분배중, 분배완료)
+	# "price" : int = 가격
+	# "each_price" : int = 분배가격
+	# "before_jungsan_ID" : list = 참석명단(분배전)
+	# "after_jungsan_ID" : list = 참석명단(분배후)
+	# "modifydate" : datetime = 수정날짜
+	# "gulid_money_insert" : bool = 혈비등록여부
+	# "bank_money_insert" : bool = 은행입금여부
+	# "image_url":""
+
+	embed = discord.Embed(
+			title = "📜 등록 정보",
+			description = "",
+			color=0x00ff00
+			)
+	embed.add_field(name = "[ 순번 ]", value = f"```{info['_id']}```")
+	embed.add_field(name = "[ 등록 ]", value = f"```{info['regist']}```")
+	embed.add_field(name = "[ 일시 ]", value = f"```{info['getdate'].strftime('%y-%m-%d %H:%M:%S')}```", inline = False)
+	embed.add_field(name = "[ 보스 ]", value = f"```{info['boss']}```")
+	embed.add_field(name = "[ 아이템 ]", value = f"```{info['item']}```")
+	embed.add_field(name = "[ 루팅 ]", value = f"```{info['toggle']}```")
+	embed.add_field(name = "[ 상태 ]", value = f"```{info['itemstatus']}```")
+	embed.add_field(name = "[ 판매금 ]", value = f"```{info['price']}```")
+	embed.add_field(name = "[ 참여자 ]", value = f"```{', '.join(info['before_jungsan_ID']+info['after_jungsan_ID'])}```")
+	if 'image_url' in info.keys():
+		if info['image_url'] is not None:
+			embed.set_image(url = info['image_url'])
+	return embed
+
 # 봇이 구동되었을 때 동작되는 코드입니다.
 @client.event
 async def on_ready():
