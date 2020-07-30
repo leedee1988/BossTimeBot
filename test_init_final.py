b6a4f27dc8c88acae19471eea90304d438f96d8b
leedee1988 <51352979+leedee1988@users.noreply.github.com>
2020-07-30 18:29:13 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -4641,6 +4641,14 @@ async def on_ready():
 	# 이 기능을 사용하여 봇의 상태를 간단하게 출력해줄 수 있습니다.
 	await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name=command[1][0], type=1), afk=False)
 
+ilsang_distribution_bot : IlsangDistributionBot = IlsangDistributionBot()
+ilsang_distribution_bot.add_cog(settingCog(ilsang_distribution_bot))
+ilsang_distribution_bot.add_cog(adminCog(ilsang_distribution_bot))
+ilsang_distribution_bot.add_cog(memberCog(ilsang_distribution_bot))
+ilsang_distribution_bot.add_cog(manageCog(ilsang_distribution_bot))
+ilsang_distribution_bot.add_cog(bankCog(ilsang_distribution_bot))
+ilsang_distribution_bot.run()
+
 while True:
 	################ 보탐봇 입장 ################ 	
 	@commands.has_permissions(manage_messages=True)
